2ba8aac7a1ab9f955ca6c665b2cb88daf48d3438
Imaccer <mcnallyian@hotmail.com>
2019-10-18 18:17:17 -0700

    test

--- assignment2/main.c
+++ assignment2/main.c
@@ -1,7 +1,19 @@
+int counter =0x0;
+
+int main() {
+  int *p_int=(int *)0x20000000;
+  ++(*p_int);
+  ++(*p_int);
+  ++(*p_int);
+  counter++;
+  return 0;
+}
+
+/*int counter=10;
 
 int main()
 {
-  int counter=0;
+  //int counter=0;
   counter++;
   counter++;
   counter++;
@@ -9,4 +21,4 @@ int main()
   counter++;
   counter++;  
     return 0;
-}
+}*/
